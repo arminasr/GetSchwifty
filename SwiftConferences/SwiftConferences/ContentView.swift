d49ec8a5288678ef104cb17dbbbee7c13cc73715
Arminas Ruzgas <arminas.ruzgas@revelsystems.com>
2019-07-29 11:54:28 +0300

    Created Xcode project

---
+++ SwiftConferences/SwiftConferences/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftConferences
+//
+//  Created by Arminas Ruzgas on 7/29/19.
+//  Copyright © 2019 Arminas Ruzgas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ SwiftConferences/SwiftConferencesTests/SwiftConferencesTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftConferencesTests.swift
+//  SwiftConferencesTests
+//
+//  Created by Arminas Ruzgas on 7/29/19.
+//  Copyright © 2019 Arminas Ruzgas. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftConferences
+
+class SwiftConferencesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
